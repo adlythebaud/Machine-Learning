5c42cfdfdbceeb75125224c5388255752545f298
Adly Thebaud <adlythebaud@gmail.com>
2018-09-29 08:23:24 -0600

    Algorithm scales values and experiments with different k values

--- Week 02/prove02.py
+++ Week 02/prove02.py
@@ -13,19 +13,26 @@ INTENTION: Create a K-Nearest Neighbors Classifier and Model,
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 from kNNClassifier import kNNClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
 
 iris = datasets.load_iris()
 x = iris.data
-#y = iris.target
-y = []
-for t in iris.target:
-    y.append(iris.target_names[t])
+y = iris.target
+#y = []
+#for t in iris.target:
+#    y.append(iris.target_names[t])
 
 
 X_train, X_test, Y_train, Y_test = train_test_split(
-        x, y, test_size = .3, random_state = 43)
+        x, y, test_size = .3, random_state = 42)
 
-kNN = kNNClassifier(5)
+# Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+kNN = kNNClassifier(4)
 model = kNN.fit(X_train, Y_train)
 
 
@@ -34,11 +41,50 @@ for xt, prediction, yt in zip(X_test, model.predict(X_test), Y_test):
 #    print(xt, iris.target_names[prediction], iris.target_names[yt])
     if prediction == yt:
         countCorrect += 1
-        
-print("Accuracy = ", round(countCorrect/len(Y_test) * 100, 2), "%")
 
+print("Dev Classifier: Accuracy = %s" % (round(countCorrect/len(Y_test) * 100, 2)))      
+
+for i in range(2,10):
+    iris = datasets.load_iris()
+    x = iris.data
+    y = iris.target
+    #y = []
+    #for t in iris.target:
+    #    y.append(iris.target_names[t])
     
     
+    X_train, X_test, Y_train, Y_test = train_test_split(
+            x, y, test_size = .3, random_state = 3)
+    
+    # Feature Scaling
+    
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+    
+    kNN = kNNClassifier(i)
+    model = kNN.fit(X_train, Y_train)
+    
+    
+    countCorrect = 0
+    for xt, prediction, yt in zip(X_test, model.predict(X_test), Y_test):
+    #    print(xt, iris.target_names[prediction], iris.target_names[yt])
+        if prediction == yt:
+            countCorrect += 1
+    classifier = KNeighborsClassifier(n_neighbors = 4)
+    KNNModel = classifier.fit(X_train, Y_train)
+    predictions = KNNModel.predict(X_test)
+    
+    classifierCountCorrect = 0
+    for xt, prediction, yt in zip(X_test, predictions, Y_test):
+    #    print(xt, iris.target_names[prediction], iris.target_names[yt])
+        if prediction == yt:
+            classifierCountCorrect += 1
+    
+    print("Dev Classifier:     k = %s, Accuracy = %s" % (i, round(countCorrect/len(Y_test) * 100, 2)))
+    print("SKLearn Classifier: k = %s, Accuracy = %s" % (i, round(classifierCountCorrect/len(Y_test) * 100, 2)))            
+    
+        
     
     
     
