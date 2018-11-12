3f55ca47a88806d89eb142ac99d0b8fbc6034cd8
Adly Thebaud <adlythebaud@gmail.com>
2018-11-12 16:38:19 -0700

    Training data split by target classes.

--- Week 07/prove07_draft/prove06_2.py
+++ Week 07/prove07_draft/prove06_2.py
@@ -38,18 +38,28 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+# 4.5 split training data based on target class
+zipped = zip(X_train, Y_train)
+training_group = []
+for i in np.unique(Y_train):
+    training_group.append([])
+    
+
+for datapoint in zipped:
+    training_group[datapoint[1]].append(np.asarray(datapoint))
+
+
 # 5. Decide how many layers and how many nodes per layer.
 
 # two layers (one hidden and one output, with 3 nodes in hidden layer)
-layers = [3,1,4]
+# this works, as long as you don't throw in a random layer with only one node in the middle.
+layers = [3,1]
 
 # 6. Create weights for each node in each layer
 weights = []
-print(np.asarray(weights).shape)
-for layer in layers:
-    
-    print("layer: ", layer)
-    
+
+for layer in layers:    
+#    print("layer: ", layer)
     # loop through each node, add a weight for every input from previous layer
     layer_weights = []
     for i in range(layer):
@@ -60,17 +70,26 @@ for layer in layers:
         # if layer.index == 0.... 
         if layers.index(layer) == 0:
             for j in range(X_train.shape[1] + 1):
-                node_weights.append(random.uniform(0,1))
+                node_weights.append(random.uniform(-1,1))
         
         else:
         # make weights for every node in PREVIOUS layer
             for j in range(layers[layers.index(layer) - 1] + 1):
-                node_weights.append(random.uniform(0,1))
+#                print("making weight from node_i ", j, " to node_j ", i)
+                node_weights.append(random.uniform(-1,1))
             
             
         layer_weights.append(node_weights)
-    print("layer_weights: ", layer_weights)
+#    print("layer_weights: ", layer_weights)
     weights.append(layer_weights)
+
+        
+# 7. set up feed forward
+# try it with training_group[0] first.
+
+for i in training_group[0]:
+    print(type(i))    
+
 
 
 
