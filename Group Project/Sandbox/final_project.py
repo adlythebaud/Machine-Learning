1c3d45ed1a26635bb77f370fee56cf90888d5055
Adly Thebaud <adlythebaud@gmail.com>
2018-12-10 23:10:40 -0700

    can generate user profile from items.

--- Group Project/Sandbox/final_project.py
+++ Group Project/Sandbox/final_project.py
@@ -10,13 +10,15 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import random
+from statistics import mean
 
 data = pd.read_csv("newest500.csv")
 
 data = data.drop(['id'], axis=1)
 data = data.iloc[:].values
 
-print(int(len(data) / 2))
+# TODO: Normalize and Scale data!
+
 # shuffle data
 indices = random.sample(range(len(data)),int(len(data) * 0.7))
 
@@ -25,9 +27,18 @@ train = data[indices]
 
 test = np.delete(data, indices, 0)
 
+# FIT DATA
+# I need the mean of each column in train, put into an array or list.
+# this is the user profile!
 
+# create user profile
+user_profile = []
+for i in range(train.shape[1]):
+    user_profile.append(mean(train[i]))
 
+print(user_profile)
 
+# C
 
 
 
